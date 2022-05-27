3105eac6274adc6f84190f146f1bf2df6a8d41b7
sumin <lsm001005@naver.com>
2022-05-27 23:08:09 +0900

    Update

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,22 +1,21 @@
 import styled from "styled-components";
 import NavigationBar from "./NavigationBar";
 
-const Title = styled.span`
-    text-align: center;
-    font-size: 37px;
-`;
 const Container = styled.div`
-    height:40vh;
+    height:35vh;
     display:flex;
     justify-content: center;
     background-color: #e6e6e6;
 `;
+const ContentContainer = styled(Container)`
+    background-color: inherit;
+    padding-top: 20px;
+`;
 const InnerContainer = styled(Container)`
-    height:40vh;
     max-width:960px;
     padding:50px;
     flex-direction:rows;
-    background-color: #8b8b8b;
+    background-color: inherit;
 `;
 
 const UserImg = styled.div`
@@ -24,12 +23,37 @@ const UserImg = styled.div`
     height:250px;
     margin-right:50px;
     border-radius: 50%;
-    background-color: pink;
+    background-color: #8b8b8b;
 `;
 const UserInfo = styled.div`
     height:250px;
     width:600px;
-    background-color: #e6e6e6;
+    display:flex;
+    justify-content: space-between;
+    flex-direction: row;
+    padding:50px ;
+`;
+const ItemBox = styled.div`
+    display:flex;
+    align-items: center;
+    flex-direction: column;
+`;
+const Item = styled.span`
+    padding-bottom:10px;
+    font-size: 18px;
+`;
+const PhotoBox = styled.div`
+    height: max-content;
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 250px auto;
+    grid-gap:10px;
+    padding-bottom: 100px;
+`;
+const Photo = styled.div`
+    height:250px;
+    width:250px;
+    background-color: #8b8b8b;
 `;
 
 function User() {
@@ -39,10 +63,34 @@ function User() {
             <Container>
                 <InnerContainer>
                     <UserImg>UserImg</UserImg>
-                    <UserInfo>UserInfo</UserInfo>
+                    <UserInfo>
+                        <ItemBox>
+                            <Item>0</Item>
+                            <Item>게시물</Item>
+                        </ItemBox>
+                        <ItemBox>
+                            <Item>0</Item>
+                            <Item>팔로워</Item>
+                        </ItemBox>
+                        <ItemBox>
+                            <Item>0</Item>
+                            <Item>팔로잉</Item>
+                        </ItemBox>
+                    </UserInfo>
                 </InnerContainer>
             </Container>
-            <Title>USER PAGE</Title>
+            <ContentContainer>
+                <PhotoBox>
+                    <Photo></Photo>
+                    <Photo></Photo>
+                    <Photo></Photo>
+                    <Photo></Photo>
+                    <Photo></Photo>
+                    <Photo></Photo>
+                    <Photo></Photo>
+                    <Photo></Photo>
+                </PhotoBox>
+            </ContentContainer>
         </>
     );
 }
