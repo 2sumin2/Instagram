0de0790adebc93a337d5ac52287332165f63b4d0
sumin <lsm001005@naver.com>
2022-06-07 00:18:54 +0900

    Update

--- src/components/User/EditUser.tsx
+++ src/components/User/EditUser.tsx
@@ -42,7 +42,7 @@ const Content = styled.div`
     padding:70px;
     display:grid;
     grid-template-columns: 1fr 3fr;
-    grid-template-rows: repeat(6, minmax(max-content, 50px));
+    grid-template-rows: minmax(max-content, 70px) repeat(5, minmax(max-content, 50px));
     gap:20px;
 `;
 
@@ -52,27 +52,50 @@ const UserImg = styled.img`
     border-radius:50%;
     height: 50px;
     width:50px;
+    justify-self: right;
+`;
+
+const EditImg = styled.button`
+    margin-left:10px;
+    height:40px;
+    color: ${props => props.theme.accentColor};
+    background:inherit;
+    font-weight:600;
+    border:0;
 `;
 
 const ItemSpan = styled.span`
     text-align: right;
+    height:40px;
+    align-self: center;
 `;
 
 const ItemInput = styled.input`
     height:max-content;
     padding:10px;
+    color: ${props => props.theme.textColor};
+    background: ${props => props.theme.bgColor};
+    border: 1px solid ${props => props.theme.textColor};
 `;
 
 const Textarea = styled.textarea`
+    min-height:40px;
     width:45vw;
+    max-width: 450px;
+    margin-bottom:6px;
     resize: vertical;
+    color: ${props => props.theme.textColor};
+    background: ${props => props.theme.bgColor};
+    border: 1px solid ${props => props.theme.textColor};
 `;
 
 const Submit = styled.button`
     background: ${props => props.theme.accentColor};
+    margin-top:50px;
     height:35px;
-    color:white;
-
+    width:100px;
+    color: ${props => props.theme.bgColor};
+    justify-self: right;
 `;
 
 function EditUser() {
@@ -88,8 +111,8 @@ function EditUser() {
                     <Content>
                         <UserImg></UserImg>
                         <div>
-                            <button>프로필 사진 바꾸기</button>
                             <ItemSpan>username</ItemSpan>
+                            <EditImg>프로필 사진 바꾸기</EditImg>
                         </div>
                         <ItemSpan>이름</ItemSpan>
                         <ItemInput></ItemInput>
@@ -101,6 +124,7 @@ function EditUser() {
                         <Textarea></Textarea>
                         <ItemSpan>이메일</ItemSpan>
                         <ItemInput></ItemInput>
+                        <div></div>
                         <Submit>제출</Submit>
                     </Content>
                 </Container>
