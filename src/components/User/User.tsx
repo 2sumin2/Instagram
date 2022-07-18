bf2e173cbbdee0c96262e0913aa8175ddceb290c
sumin <lsm001005@naver.com>
2022-07-19 00:33:09 +0900

    Update

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -14,6 +14,7 @@ const Container = styled.div`
     display:flex;
     flex-direction: column;
     align-items: center;
+    padding: 100px 0;
 `;
 const ContentContainer = styled.div`
     background-color: inherit;
